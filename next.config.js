02f3b429133237f2ed8abfdb377ac117032f8550
f9260 <wtfthisvideo@gmail.com>
2024-06-12 20:04:02 +0300

    olacak

---
+++ next.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  output: 'export',
+  distDir: 'dist',
+  images: {
+    unoptimized:true,
+    domains: ["localhost"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+        port: "",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
