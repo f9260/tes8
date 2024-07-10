8d43f658af9f7083c2c7eb12b960b505db10f4aa
f9260 <161130407+f9260@users.noreply.github.com>
2024-07-10 19:26:58 +0300

    Update index.tsx

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -23,7 +23,7 @@
                     <Link
                       href="https://shopier.com/23747500"
                       target="_blank"
-                      className="rounded-sm bg-green-500 px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-green-600"
+                      className="rounded-sm bg-[#00b22d] px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-green-500"
                     >
                       Satın Al
                     </Link>
