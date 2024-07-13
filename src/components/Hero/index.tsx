9fa45e5fd07fed556cdf1098e3a2b6fa9ca63c71
f9260 <161130407+f9260@users.noreply.github.com>
2024-07-13 12:30:47 +0300

    Update index.tsx

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -19,15 +19,29 @@
                     
                     Ayarlarınızı yapın ve çalıştırın, gerisini ona bırakın.
                   </p>
-                  <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
-                    <Link
-                      href="https://shopier.com/23747500"
-                      target="_blank"
-                      className="rounded-sm bg-[#00b22d] px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-green-500"
-                    >
-                      Satın Al
-                    </Link>
-                   
+                                    <div class="flex items-center justify-center">
+                    <div class="flex flex-row items-center justify-center space-x-4 sm:space-x-0 sm:space-y-0 sm:flex-col sm:flex-row sm:justify-start">
+                      <Link
+                        href="https://rehber.ve3.plus"
+                        class="inline-block rounded-sm bg-black px-3 sm:px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-black/90 dark:bg-white/10 dark:text-white dark:hover:bg-white/5 sm:hidden"
+                      >
+                        Rehber
+                      </Link>
+                      <Link
+                        href="https://shopier.com/23747500"
+                        target="_blank"
+                        class="rounded-sm bg-green-500 px-3 sm:px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-green-600"
+                      >
+                        Satın Al
+                      </Link>
+                      <Link
+                        href="https://forum.ve3.plus/d/5-demo-talebi"
+                        target="_blank"
+                        class="inline-block rounded-sm bg-black px-3 sm:px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-black/90 dark:bg-white/10 dark:text-white dark:hover:bg-white/5 sm:hidden"
+                      >
+                        Demo
+                      </Link>
+                    </div>
                   </div>
                 </div>
               </div>
