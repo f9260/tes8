ff5f57ea283a85cce7e5ceafd0f1f50baea7b572
f9260 <161130407+f9260@users.noreply.github.com>
2024-09-04 19:18:28 +0300

    Update index.tsx

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -14,11 +14,11 @@ const Hero = () => {
             <div className="w-full px-4">
               <div className="mx-auto max-w-[800px] text-center">
                 <h1 className="mb-5 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
-                  VE3.Plus - Unlimited Video Editing Automation
+                  VE3.Plus - Sınırsız Video Düzenleme Otomasyonu
                 </h1>
                 <p className="mb-12 text-base !leading-relaxed text-body-color dark:text-body-color-dark sm:text-lg md:text-xl">
-                  Create unlimited videos without any video editing skills and start making money on social media.
-                  Make a few simple settings in seconds, run it, and let the automation do the rest.
+                  Video düzenleme bilgisine ihtiyaç duymadan sınırsız sayıda benzersiz video oluşturun ve sosyal medyada para kazanmaya başlayın. 
+                  Saniyeler süren birkaç basit ayar yapın, çalıştırın ve gerisini otomasyona bırakın.
                 </p>
                 <div className="flex flex-col items-center justify-center space-y-4">
                   <div className="flex flex-row items-center justify-center space-x-4 w-full max-w-md">
