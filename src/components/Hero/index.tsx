0153e774b5f6fa5deabd8a6ced463d463ed90a10
f9260 <161130407+f9260@users.noreply.github.com>
2024-09-04 19:02:45 +0300

    Update index.tsx

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -37,7 +37,7 @@ const Hero = () => {
                       Topluluk
                     </button>
                     <button
-                      onClick={() => window.open('https://onedrive.live.com/download?resid=6F652AD40ADCFC4D%21116&authkey=%21AL%2DBR3IUIKoTnlQ', '_blank')}
+                      onClick={() => window.open('https://onedrive.live.com/download?resid=6F652AD40ADCFC4D%21116&authkey=%21AL%2DBR3IUIKoTnlQ')}
                       className="flex-1 rounded-sm bg-gradient-to-r from-[#FF0000] to-[#C71585] px-3 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-gradient-to-r hover:from-[#E60000] hover:to-[#A0136D] dark:text-white dark:hover:from-[#E60000] dark:hover:to-[#A0136D] text-center cursor-pointer"
                     >
                       İndir ⤓ 
